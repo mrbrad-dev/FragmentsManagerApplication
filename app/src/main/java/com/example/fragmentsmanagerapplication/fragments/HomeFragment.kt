8e8e1964290419562f51b3e2f40fbfd0762a2ed2
mrbrad-dev <bradouyaiakpos@gmail.com>
2021-07-14 14:30:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fragmentsmanagerapplication/fragments/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.fragmentsmanagerapplication.fragments
+
+import android.os.Bundle
+import android.renderscript.ScriptGroup
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fragmentsmanagerapplication.R
+import com.example.fragmentsmanagerapplication.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
